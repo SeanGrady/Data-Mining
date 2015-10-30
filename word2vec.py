8a70fcd9e7c0c0c970421ab7dadfea7293d1b254
Sean Grady <seangrady@Seans-MacBook-Pro-3.local>
2015-10-30 12:35:17 -0700

    Sentance builder runs properly now, everything should work but still need to test model.

--- word2vec.py
+++ word2vec.py
@@ -12,14 +12,17 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 punctuation = set(string.punctuation)
+replace_punctuation = string.maketrans(string.punctuation,
+                                       ' '*len(string.punctuation))
+
 
 def yield_data(fname):
     for l in open(fname):
         yield literal_eval(l)
 
 def make_wordlist(sentance):
-    r = ''.join([c for c in sentance if not c in punctuation])
-    word_list = r.split()
+    sans_punct = sentance.translate(replace_punctuation)
+    word_list = sans_punct.split()
     return word_list
 
 def make_sentance_list(review):
@@ -30,12 +33,13 @@ def make_sentance_list(review):
                  if len(sentance) > 0]
     return sentances
 
-def list_review_sentances(data):
+def list_review_sentances(fname):
     sentances = []
-    for d in data:
-        review_text = d['reviewText']
-        sentances.extend(make_sentance_list(review_text))
-	return sentances
+    for data in yield_data(fname):
+        review_text = data['reviewText']
+        new_sentances = make_sentance_list(review_text)
+        sentances = sentances + new_sentances
+    return sentances
 
 num_features = 500
 min_word_count = 40
@@ -43,9 +47,9 @@ num_workers = 4
 context = 10
 downsampling = 1e-3
 
+print "Building sentance list..."
+sentances = list_review_sentances('train.json')
 print "Training model..."
-sentances = list_review_sentances(yield_data('train.json'))
-print sentances[0]
 model = word2vec.Word2Vec(sentances, workers=num_workers, size=num_features,
                           min_count=min_word_count, window=context,
                           sample=downsampling)
