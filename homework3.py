233f2a30b32597a09c4bc5f71f50d09035c59c22
Sean Grady <vedicmonk@gmail.com>
2015-11-04 14:49:58 -0800

    Problem 1.3 working, need to do 1.4

--- homework3.py
+++ homework3.py
@@ -7,25 +7,25 @@ import scipy.optimize
 from collections import deque
 import gc
 
-def line_generator(fname):
-    for line in open(fname):
-        yield literal_eval(line)
-
-def load_fields(fname, fields):
-    helpful = []
+def line_generator(fname, start, end):
     i = 0
-    for line in line_generator(fname):
-        if i % 1000 == 0: print i
+    for line in open(fname):
+        if i >= start and i <= end:
+            yield literal_eval(line)
+        i += 1
+        if i > end: break
+
+def load_fields(fname, fields, start, end):
+    helpful = []
+    for line in line_generator(fname, start, end):
         line_dict = dict((field, line[field]) for field in fields)
         helpful.append(line_dict)
-        i += 1
     return helpful
 
 fields = ['helpful', 'reviewText', 'rating']
-data = load_fields('train.json', fields)
-
-train_data = data[:100000]
-valid_data = data[900000:]
+#data = load_fields('train.json', fields)
+train_data = load_fields('train.json', fields, 0, 100000)
+valid_data = load_fields('train.json', fields, 900000, 1000000)
 
 train_features = [{'nHelpful':rev['helpful']['nHelpful'],
                    'outOf':rev['helpful']['outOf'],
@@ -40,7 +40,7 @@ valid_features = [{'nHelpful':rev['helpful']['nHelpful'],
 train_alpha = sum([rev['ratio'] for rev in train_features])/len(train_features)
 
 tot_error = 0
-for review in valid_data:
+for review in valid_dat:
     nHelpful = review['helpful']['nHelpful']
     outOf = review['helpful']['outOf']
     prediction = round(train_alpha * outOf)
@@ -53,12 +53,19 @@ def construct_feature(review):
     r = ''.join([c for c in review['reviewText'].lower() if not c in punctuation])
     num_words = len(r.split())
     rating = review['rating']
-    feature = [1, num_words, rating]
+    feature = [1.0, num_words, rating]
     return feature
 
 X = [construct_feature(review) for review in train_data]
-y = [(float(rev['helpful']['nHelpful'])/rev['helpful']['outOf'])
-     for review in train_data]
+y = []
+for review in train_data:
+    outOf = review['helpful']['outOf']
+    nHelpful = review['helpful']['nHelpful']
+    if outOf > 0:
+        ratio = float(nHelpful)/outOf
+    else:
+        ratio = 0.0
+    y.append(ratio)
 
 theta, residuals, rank, s = numpy.linalg.lstsq(X, y)
 
